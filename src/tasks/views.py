2103617cdd7531fd0e20634a5aa6c3e933d6c414
dimaboro-code <dimka.borovikov@gmail.com>
2025-06-01 18:40:29 +0500

    add pagination

--- src/tasks/views.py
+++ src/tasks/views.py
@@ -2,11 +2,19 @@ from rest_framework import viewsets, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class TaskPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
+    pagination_class = TaskPagination
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['title', 'completed']
 
